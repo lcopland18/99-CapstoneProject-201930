de9aecefc7e6157cf67b8f31d30f6e0514070701
lcopland18 <coplanlm@rose-hulman.edu>
2019-05-09 09:43:10 -0400

    Changed robot number

--- src/m0_run_this_on_LAPTOP.py
+++ src/m0_run_this_on_LAPTOP.py
@@ -51,7 +51,7 @@ def main():
     mqtt_sender = mqtt.MqttClient(delegate)
     delegate.set_mqtt_sender(mqtt_sender)
 
-    mqtt_sender.connect_to_ev3(lego_robot_number = 04)
+    mqtt_sender.connect_to_ev3(lego_robot_number = 4)
     # DONE 3: Replace 99 in the above by YOUR team's robot number.
 
     # -------------------------------------------------------------------------
