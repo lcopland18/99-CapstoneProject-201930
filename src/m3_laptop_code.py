9feaf687f45890de876e79ef350c65cbdffb2d19
hammonr1 <hammonr1@rose-hulman.edu>
2019-05-15 09:47:09 -0400

    mqtt_sender works

--- src/m3_laptop_code.py
+++ src/m3_laptop_code.py
@@ -20,32 +20,34 @@ def get_my_frame(root, window, mqtt_sender):
     # Construct your frame:
     frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
     frame_label = ttk.Label(frame, text="Ruth Hammond")
-    frame_label.grid()
+    frame_label.grid(row =0,column=1)
     # TODO 2: Put your name in the above.
 
-    armspeed_button = ttk.Button(frame, text="Arm Speed:")
+    armspeed_button = ttk.Label(frame, text="Arm Speed:")
     armspeed_button.grid(row = 1, column = 0 )
     armspeed_entry = ttk.Entry(frame, width=8)
     armspeed_entry.insert(0, "100")
-    armspeed_entry.grid(row = 1, column = 1)
+    armspeed_entry.grid(row = 2, column = 0)
 
-    armto_button = ttk.Button(frame, text="Arm To:")
-    armto_button.grid(row = 2, column = 0)
+    armto_button = ttk.Button(frame, text="Go To Position")
+    armto_button.grid(row = 1, column = 2)
+    position_label = ttk.Label(frame, text="Position:")
+    position_label.grid(row=3, column = 0)
     armto_entry = ttk.Entry(frame,width = 8)
     armto_entry.insert(0, "100")
-    armto_entry.grid(row=2, column=1)
+    armto_entry.grid(row=4, column=0)
     armto_button["command"] = lambda: handle_move_arm_to_position(armspeed_entry,armto_entry,mqtt_sender)
 
     armcalibrate_button = ttk.Button(frame, text="Calibrate Arm")
-    armcalibrate_button.grid(row = 5)
+    armcalibrate_button.grid(row = 4,column = 2)
     armcalibrate_button["command"] = lambda: handle_calibrate(mqtt_sender)
 
     armup_button = ttk.Button(frame, text="Arm Up")
-    armup_button.grid(row = 3, column = 0 )
+    armup_button.grid(row = 2, column = 2)
     armup_button["command"] = lambda: handle_arm_up(armspeed_entry,mqtt_sender)
 
     armdown_button = ttk.Button(frame, text="Arm Down")
-    armdown_button.grid(row =3,column = 1 )
+    armdown_button.grid(row =3,column = 2 )
 
 
 
