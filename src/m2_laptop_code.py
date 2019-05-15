8ffbfeb720167fe987046ecc2006222708c8943e
lcopland18 <coplanlm@rose-hulman.edu>
2019-05-15 09:41:32 -0400

    Kind of works

--- src/m2_laptop_code.py
+++ src/m2_laptop_code.py
@@ -20,17 +20,36 @@ def get_my_frame(root, window, mqtt_sender):
     # Construct your frame:
     frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
     frame_label = ttk.Label(frame, text="Lauren Copland")
-    frame_label.grid(column = 2, row = 0)
+    frame_label.grid(column = 1, row = 0)
     # DONE 2: Put your name in the above.
 
     # Add the rest of your GUI to your frame:
     # TODO: Put your GUI onto your frame (using sub-frames if you wish).
 
-    left = ttk.Button(frame,text = "Spin Left" )
-    left.grid(column =0, row = 1)
+    #Buttons and Entry Boxes
+    spin_left_button = ttk.Button(frame,text = "Spin Left" )
+    spin_right_button = ttk.Button(frame,text = "Spin Right")
 
-    right = ttk.Button(frame,text = "Spin Right")
-    right.grid(column = 3, row = 1)
+    spin_left_right_entry = ttk.Entry(frame,width = 8)
+    spin_left_right_label = ttk.Label(frame, text = "Spin 0° to 360°")
+    spin_left_right_entry.insert(0,'180')
+    spin_speed_entry = ttk.Entry(frame,width = 8)
+    spin_speed_label = ttk.Label(frame,text = "Spin Speed 0 to 100")
+    spin_speed_entry.insert(0,'100')
+
+    #Grid
+    spin_left_button.grid(column =0, row = 3)
+    spin_right_button.grid(column = 2, row = 3)
+
+    spin_left_right_label.grid(column = 1, row = 2)
+    spin_left_right_entry.grid(column = 1, row = 3)
+
+    spin_speed_entry.grid(column = 1 , row =  5)
+    spin_speed_label.grid(column = 1, row = 4 )
+
+    #Lambda Functions
+    spin_left_button['command'] = lambda: handle_spin_left(spin_speed_entry,spin_left_right_entry,mqtt_sender)
+
 
     # Return your frame:
     return frame
@@ -53,3 +72,16 @@ class MyLaptopDelegate(object):
 
 
 # TODO: Add functions here as needed.
+
+# def spin(mqtt_sender,left_speed,right_speed,degrees):
+#     print("Sending message to the robot to spin at speed:",left_speed,right_speed)
+#     print("Sending message to the robot to spin:",degrees,"degrees")
+#     mqtt_sender.send_message("spin",[left_speed,right_speed,degrees])
+
+
+
+def handle_spin_left(spin_speed_entry,spin_distance_deg,mqtt_sender):
+    print('Spin Left Message:', spin_speed_entry.get(),spin_distance_deg.get())
+    speed = int(spin_speed_entry.get())
+    distance = int(spin_distance_deg.get())
+    mqtt_sender.send_message('spin_left',[-speed,speed,distance])
